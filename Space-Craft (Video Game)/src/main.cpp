3417726a8e0d17c20548347359b7ce6f27d7cac4
AdkhamjonKhamidov <136289251+AdkhamjonKhamidov@users.noreply.github.com>
2025-04-04 10:33:42 -0400

    deleted stuff - restarting proj

--- Space-Craft (Video Game)/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-//in not working, double check Makefile and CMakeLists.txt
-#include <SDL.h>
-
-int main() {
-    std::cout << "Hello, Space-Craft!" << std::endl;
-    return 0;
-}
